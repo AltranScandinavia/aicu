0b36dbc74061f5dc4ac39f2284e5d0fe9b5f8755
thomaspringle <pringle.thomas@gmail.com>
2013-11-11 00:04:57 +0100

    Added Internal News, Events and Forum

--- background.js
+++ background.js
@@ -1,37 +1,54 @@
-// SP OBJ:
-// xml.SPXML.d.SocialFeed.Threads.results[0].RootPost.Text
-// xml.SPXML.d.SocialFeed.Threads.results[0].RootPost.ModifiedTime
-// var date = new Date(xml.SPXML.d.SocialFeed.Threads.results[0].RootPost.ModifiedTime)
 
-var BASE_URL = 'https://altranintranet.sharepoint.com';
-var NEWS_FEED = '/_api/social.feed/my/news';
-var backgroundPoller = {
+var KEYS = {personalNewsFeed: 'PERSONAL_NEWS_FEED', internalNews: 'INTERNAL_NEWS_FEED', altranForum: 'ALTRAN_FORUM', altranEvents: 'ALTRAN_EVENT_CALENDAR'};
 
-  requestXML: function() {
+var BASE_URL      = 'https://altranintranet.sharepoint.com';
 
+var PERSONAL_NEWS_FEED = '/_api/social.feed/my/news';
+var INTERNAL_NEWS_FEED = "/news/_api/web/lists/getbytitle('posts')/items?$orderby=Modified desc";
+var ALTRAN_FORUM       = "/AltranForum/_api/web/lists/getbytitle('Discussions List')/items?$orderby=Modified desc";
+var ALTRAN_EVENTS      = "/AltranEventCalendar/_api/web/lists/getbytitle('EventCalendar')/items?$orderby=Modified desc";
+
+
+function BackgroundPoller () {
+
+};
+
+BackgroundPoller.prototype.incrementBadgeText = function() {
+  chrome.browserAction.getBadgeText({}, function (text) {
+    var badgeText = '1';
+    if (!isNaN(parseInt(text))) {
+      badgeText = parseInt(text, 10) + parseInt(badgeText, 10);
+    }
+    chrome.browserAction.setBadgeText({'text':''+badgeText});
+  });
+};
+
+BackgroundPoller.prototype.fetchData = function (URL, callback) {
   var me = this;
-    $.ajax({
-      'url': BASE_URL + NEWS_FEED,
-      'type': 'GET',
-      'dataType': 'json',
-      'success': function(data) { 
-        me.checkNewsFeed.call(me, data);
-      },
-      'error': function(err) { console.log('ERROR!'); console.log(err); },
-      'beforeSend': setHeader
-    });
-    function setHeader(xhr) {
-      xhr.setRequestHeader('Accept', 'application/json;odata=verbose');
-    }    
-  },
+  $.ajax({
+    'url': URL,
+    'type': 'GET',
+    'dataType': 'json',
+    'success': function(data) { 
+      callback(data);
+    },
+    'error': function(err) { console.log('ERROR!'); console.log(err); },
+    'beforeSend': setHeader
+  });
+  function setHeader(xhr) {
+    xhr.setRequestHeader('Accept', 'application/json;odata=verbose');
+  } 
+};
+
+BackgroundPoller.prototype.checkPersonalNewsFeed = function () {
+  var me = this;
   
-  checkNewsFeed: function (data) {
-    var me = this;
+  function checkPersonalNewsFeedData(data) {
     var latestUpdate = data.d.SocialFeed.NewestProcessed;
 
-    chrome.storage.local.get('NewsFeed', function (newsFeedItems) {
+    chrome.storage.local.get('PERSONAL_NEWS_FEED', function (newsFeedItems) {
       var lastStoredDate = new Date();
-      var newsFeedItem = newsFeedItems['NewsFeed'];
+      var newsFeedItem = newsFeedItems['PERSONAL_NEWS_FEED'];
       
       if (newsFeedItem && newsFeedItem.newestProcessed) {
         lastStoredDate = new Date(newsFeedItem.newestProcessed);
@@ -41,20 +58,83 @@ var backgroundPoller = {
       if (lastStoredDate.toString() != lastPolledDate.toString()) {
         console.log('new item found, display notification');
         newsFeedItem = { 'newItemFound': true, 'newestProcessed': lastPolledDate.toString()};
-        chrome.storage.local.set({'NewsFeed': newsFeedItem}, function() {
+        chrome.storage.local.set({'PERSONAL_NEWS_FEED': newsFeedItem}, function() {
           if (chrome.extension.lastError) {
             console.log('An error occurred: ' + chrome.extension.lastError.message);
           }
         });
-        chrome.browserAction.setBadgeText({'text':'1'});
+        me.incrementBadgeText();
+       
       }
     });
   }
+  me.fetchData(BASE_URL + PERSONAL_NEWS_FEED, checkPersonalNewsFeedData);
 };
 
+BackgroundPoller.prototype.checkInternalNewsFeed = function () {
+  var me = this;
+  me.checkList.call(me, 'INTERNAL_NEWS_FEED', BASE_URL + INTERNAL_NEWS_FEED);
+
+};
+ 
+BackgroundPoller.prototype.checkAltranForum = function () {
+  var me = this;
+  me.checkList.call(me, 'ALTRAN_FORUM', BASE_URL + ALTRAN_FORUM);
+
+};
+
+BackgroundPoller.prototype.checkAltranEvents = function () {
+  var me = this;
+  me.checkList.call(me, 'ALTRAN_EVENT_CALENDAR', BASE_URL + ALTRAN_EVENTS);
+
+};
+BackgroundPoller.prototype.checkList = function (storageKey, listUrl) {
+  var me = this;
+  
+  function checkListDataForChanges(data) {
+    var latestUpdate = data.d.results[0].Modified;
+    
+    chrome.storage.local.get(storageKey, function (listDataItems) {
+      var lastStoredDate = new Date();
+      var newsFeedItem = listDataItems[storageKey];
+
+      if (newsFeedItem && newsFeedItem.newestProcessed) {
+        lastStoredDate = new Date(newsFeedItem.newestProcessed);
+      }
+      var lastPolledDate = new Date(data.d.results[0].Modified);
+
+      if (lastStoredDate.toString() != lastPolledDate.toString()) {
+        newsFeedItem = { 'newItemFound': true, 'newestProcessed': lastPolledDate.toString()};
+        var itemData = {};
+        itemData[storageKey] = newsFeedItem;
+        chrome.storage.local.set(itemData, function() {
+          if (chrome.extension.lastError) {
+            console.log('An error occurred: ' + chrome.extension.lastError.message);
+          }
+        });
+        me.incrementBadgeText();
+      }
+    });
+  }
+  me.fetchData(listUrl, checkListDataForChanges);
+};
+
+var backgroundPoller = new BackgroundPoller();
+
 function pollSP() {
-  console.log('requesting XML');
-  backgroundPoller.requestXML();
+
+  console.log('requesting personal DATA');
+  backgroundPoller.checkPersonalNewsFeed();
+  
+  console.log('requesting internal DATA');
+  backgroundPoller.checkInternalNewsFeed();
+  
+  console.log('requesting forum DATA');
+  backgroundPoller.checkAltranForum();
+
+  console.log('requesting event DATA');
+  backgroundPoller.checkAltranEvents();
+  
   setTimeout(function() {
     pollSP();
   }, 10*60*1000);
--- popup.js
+++ popup.js
@@ -1,22 +1,31 @@
 // Shows the new item and removed the badge from the extension icon
+var KEYS = {personalNewsFeed: 'PERSONAL_NEWS_FEED', internalNews: 'INTERNAL_NEWS_FEED', altranForum: 'ALTRAN_FORUM', altranEvents: 'ALTRAN_EVENT_CALENDAR'};
+
+var decrementBadgeText = function() {
+  chrome.browserAction.getBadgeText(function (text) {
+    var badgeText = '';
+    if (!isNaN(paraseInt(text))) {
+      badgeText = (parseInt(text, 10)-1);
+      badgeText = badgeText > 0 ? badgeText : '';
+    }
+    chrome.browserAction.setBadgeText({'text':''+badgeText});
+  });
+};
 
 var showUnreadLists = {
-  showNewsFeed: function (e) {
+  showPersonalNewsFeed: function () {
     var me = this;
-    chrome.storage.local.get('NewsFeed', function (newsFeedItems) {
-      var newsFeedItem = newsFeedItems['NewsFeed'];
-      console.log(newsFeedItem);
+    chrome.storage.local.get('PERSONAL_NEWS_FEED', function (newsFeedItems) {
+      var newsFeedItem = newsFeedItems['PERSONAL_NEWS_FEED'];
       
-      var linkText = 'News Feed';
-      if (newsFeedItem !== undefined && newsFeedItem.newItemFound == false) {
-        linkText = 'No News';
-      }
-      me.createLink.call(me, linkText);
+      var newItemsFound = !(newsFeedItem !== undefined && newsFeedItem.newItemFound == false);
+
+      me.createLink.call(me, newItemsFound, 'personal');
 
       var lastStoredDate = (!newsFeedItem || !newsFeedItem.newestProcessed) ? new Date().toString : newsFeedItem.newestProcessed;
       newsFeedItem = { 'newItemFound': false, 'newestProcessed': lastStoredDate};
       
-      chrome.storage.local.set({'NewsFeed': newsFeedItem}, function() {
+      chrome.storage.local.set({'PERSONAL_NEWS_FEED': newsFeedItem}, function() {
         if (chrome.extension.lastError) {
           console.log('An error occurred: ' + chrome.extension.lastError.message);
         }
@@ -24,17 +33,56 @@ var showUnreadLists = {
       chrome.browserAction.setBadgeText({'text':''});
     });
   },
-  createLink: function (linkText) {
-    var div = document.createElement('div');
-    var anchor = document.createElement('a');
-    anchor.innerHTML = linkText;
-    anchor.href = 'https://altranintranet.sharepoint.com';
-    anchor.target = '_blank';
-    div.appendChild(anchor);
-    document.body.appendChild(div); 
+  showInternalNewsFeed: function () {
+    var me = this;
+    me.showLinkForItem.call(me, 'INTERNAL_NEWS_FEED', 'internal');
+
+  },  
+  showAltranForum: function () {
+    var me = this;
+    me.showLinkForItem.call(me, 'ALTRAN_FORUM', 'forum');
+
+  },    
+  
+  showAltranEvents: function () {
+    var me = this;
+    me.showLinkForItem.call(me, 'ALTRAN_EVENT_CALENDAR', 'event');
+  },  
+  showLinkForItem: function (storageKey, linkName) {
+    var me = this;
+    chrome.storage.local.get(storageKey, function (newsFeedItems) {
+      var newsFeedItem = newsFeedItems[storageKey];
+      
+      var newItemsFound = !(newsFeedItem !== undefined && newsFeedItem.newItemFound == false);
+      me.createLink.call(me, newItemsFound, linkName);
+
+      var lastStoredDate = (!newsFeedItem || !newsFeedItem.newestProcessed) ? new Date().toString : newsFeedItem.newestProcessed;
+      newsFeedItem = { 'newItemFound': false, 'newestProcessed': lastStoredDate};
+      var data = {};
+      data[storageKey] = newsFeedItem;
+      chrome.storage.local.set(data, function() {
+        if (chrome.extension.lastError) {
+          console.log('An error occurred: ' + chrome.extension.lastError.message);
+        }
+      });
+      chrome.browserAction.setBadgeText({'text':''});
+    });
+  },  
+
+  
+  createLink: function (newItemsFound, linkName) {
+    var link = document.getElementById(linkName+'-link');
+    if (newItemsFound) {
+      link.classList.add('new_items_found');
+    } else {
+      link.classList.remove('new_items_found');
+    }
   }
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-  showUnreadLists.showNewsFeed();
+  showUnreadLists.showPersonalNewsFeed();
+  showUnreadLists.showInternalNewsFeed();
+  showUnreadLists.showAltranForum();
+  showUnreadLists.showAltranEvents();
 });
